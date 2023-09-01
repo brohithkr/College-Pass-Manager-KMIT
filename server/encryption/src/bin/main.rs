257dbfe0bc5aae7f714c95ce11717abc0e8183bf
brohithkr <brohithkr@gmail.com>
2023-09-01 20:29:55 +0530

    encryption module

--- server/encryption/src/bin/main.rs
+++ server/encryption/src/bin/main.rs
@@ -1,17 +1,25 @@
+#![allow(unused_variables)]
+#![allow(dead_code)]
+#![allow(unused_imports)]
+
 use encryption::*;
-
-use openssl::rsa::{Rsa,Padding};
-
+// use openssl::rsa::{Rsa,Padding};
 
 fn main() {
     let (pri_str, pub_str) = rsa_generate();
 
-    println!("{:?}",(&pri_str, &pub_str));
-    let pub_rsa = Rsa::public_key_from_pem(pub_str.as_bytes()).unwrap();
+    // println!("{:?}",(&pri_str, &pub_str));
+    // let pub_rsa = Rsa::public_key_from_pem(pub_str.as_bytes()).unwrap();
 
-    let msg = b"helllo dude";
-    let mut endata = vec![0; pub_rsa.size() as usize];
-    let res = pub_rsa.public_encrypt(msg, &mut endata, Padding::PKCS1);
+    let msg = String::from("i am the danger, the one who knocks!");
+    // let mut endata = vec![0; pub_rsa.size() as usize];
+    // let res = pub_rsa.public_encrypt(msg, &mut endata, Padding::PKCS1);
     
+    let enmsg = rsa_encrypt(&pri_str, &msg).unwrap();
+    println!("Encrypted: {}",enmsg.len());
 
+    let demsg = rsa_decrypt(&pub_str, &enmsg).unwrap();
+    // assert!(msg == demsg);
+    println!("Decrypted: {} {}",demsg, msg)
 }
+
--- server/encryption/src/lib.rs
+++ server/encryption/src/lib.rs
@@ -1,4 +1,7 @@
-use openssl::rsa::{Padding, Rsa};
+#![allow(unused_variables)]
+#![allow(dead_code)]
+#![allow(unused_imports)]
+use openssl::{rsa::{ Rsa ,Padding }, error::ErrorStack};
 use base64::{Engine as _, engine::general_purpose};
 
 fn b64_encode(data: Vec<u8>) -> String {general_purpose::STANDARD.encode(data)}
@@ -11,4 +14,41 @@ pub fn rsa_generate() -> (String, String) {
     let pri_str = String::from_utf8(pri_key).unwrap();
     let pub_str = String::from_utf8(pub_key).unwrap();
     (pri_str,pub_str)
+}
+
+
+pub fn rsa_encrypt(private_key_pem: &String, data: &String) -> Result<String,ErrorStack> {
+    let private_key =  Rsa::private_key_from_pem(
+        private_key_pem.as_bytes()
+    )?;
+
+    let mut endata = vec![0; private_key.size() as usize];
+
+    private_key.private_encrypt(
+        data.to_owned().as_bytes(),
+        &mut endata,
+        Padding::PKCS1
+    )?;
+
+    Ok(
+        b64_encode(endata)
+    )
+}
+
+pub fn rsa_decrypt(public_key_pem: &String, endata: &String) -> Result<String, ErrorStack> {
+    let public_key = Rsa::public_key_from_pem(
+        public_key_pem.as_bytes()
+    )?;
+
+    let mut data = vec![0; public_key.size() as usize];
+    
+    public_key.public_decrypt(
+        &b64_decode(endata.to_owned()),
+        &mut data, 
+        Padding::PKCS1
+    )?;
+
+    Ok(
+        String::from_utf8(data).unwrap()
+    )
 }
