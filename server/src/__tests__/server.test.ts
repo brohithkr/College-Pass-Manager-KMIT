2f81a73f0a5675b45570dabfc5eedf1bda0014cb
brohithkr <brohithkr@gmail.com>
2023-09-18 22:19:22 +0530

    db connectors and their tests

--- server/src/__tests__/server.test.ts
+++ server/src/__tests__/server.test.ts
@@ -1,15 +1,30 @@
 import {describe, test, expect} from "bun:test";
 import * as db_connector from "../db_connector";
 import Database from "bun:sqlite";
+import * as types from "../types";
+import { Verify } from "crypto";
 
-var db: Database | null = null;
 
 describe(
     "DB Connector", () => {
+        var db: Database | null = null;
         test(
-            "create", () => {
-                db = 
+            "connect", () => {
+                db = db_connector.connect();
             }
         )
+        test(
+            "create", () => {
+                let john: types.Verifier = {
+                    name: "John Wick",
+                    uid: "johnwick",
+                    passwd: "johny123",
+                }
+                db_connector.create(db as Database, "Verifier", john);
+            }
+        )
+        // test(
+        //     "update", 
+        // )
     }
 )
--- server/src/db_connector.ts
+++ server/src/db_connector.ts
@@ -1,4 +1,5 @@
 import {Database} from "bun:sqlite";
+import { expect } from "bun:test";
 
 // import {Verifier} from "./types.ts";
 
@@ -7,44 +8,73 @@ var db_path = "DB/data.db";
 
 function initialize_db() {
     const db = new Database(db_path, {create: true});
-    let schema = {
+    let schema: any = {
         "Verifier": `
         CREATE TABLE Verifier (
-            uid varchar(30),
+            uid varchar(30) UNIQUE,
             name varchar(50),
             passwd varchar(30)
         )
         `,
-        "Pass": `
+        "Issued_Pass": `
+        CREATE TABLE Issued_Pass (
             roll_no varchar(11),
             issue_date varchar(10),
             valid_till varchar(10),
             pass_type varchar(15),
             b64_img text
+        )
         `
+    };
+    for (let i in (schema)) {
+        try{
+            db.run(schema[i]);
+            console.log(`Table ${i} created.`)
+        } catch(e) {
+            console.log(`table ${i} already exists.`)
+            continue;
+        }
     }
 }
 
-function connect(){
-    db 
+function connect(): Database{
+    return new Database(db_path);
 }
 
 function create(db: Database, table: string, data: object) {
+    let quote_wrapper = (lst: string[]) => {
+        var wrapped_str: string = "";
+        for(let i=0;i<lst.length;i++){
+            wrapped_str += `'${lst[i]}',`;
+        }
+        return wrapped_str.substring(0,wrapped_str.length-1);
+    }
     let keys = String(Object.keys(data));
-    let values = String(Object.values(data));
-    let cmd = `INSERT INTO ${table}(${keys}) VALUES (${values})`;
+    let values = quote_wrapper(Object.values(data));
+    let cmd = `INSERT INTO ${table}(${keys}) VALUES (${values});`;
     db.run(cmd);
 }
 
 function read(
-    db: Database, 
-    table: String, 
-    identifier: string, 
+    db: Database,
+    table: String,
+    identifier: string,
     identifier_value: string,
     return_values: Array<String>
     ) {
     let val_str = String(return_values);
-    let cmd = `SELECT (${val_str}) FROM ${table} WHERE ${identifier}=${identifier_value}`;
+    let cmd = `SELECT (${val_str}) FROM ${table} WHERE ${identifier}='${identifier_value}'`;
     let query = db.query(cmd);
     return query.all()
 }
+
+initialize_db();
+
+
+
+export {
+    initialize_db, 
+    create, 
+    read, 
+    connect
+};
