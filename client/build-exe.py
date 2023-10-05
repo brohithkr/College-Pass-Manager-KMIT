3cb3a2d8f04204fba976994e6a857e01d2547cf9
Jai-JAP <jai.jap.318@gmail.com>
2023-10-05 21:01:09 +0530

    Fix freezing script

--- client/build-exe.py
+++ client/build-exe.py
@@ -22,10 +22,10 @@ elif ostype == "Darwin":
     extras = f"-i {joinpath('.', 'client', 'res', 'kmit.icns')} "
     iconext = 'icns'
 
-args = f"./pass-generator/main.py -Fw{strip} -n generator " + \
-    f"--add-data {joinpath('.', 'client', 'res', 'design.ui')}{delim}data " + \
-    f"--add-data {joinpath('.', 'client', 'res', 'Gear.jpg')}{delim}data " + \
-    f"--add-data {joinpath('.', 'client', 'res', f'kmit.{iconext}')}{delim}data " \
+args = f"./client/main.py -Fw{strip} -n generator " + \
+    f"--add-data {joinpath('.', 'client', 'res', 'design.ui')}{delim}res " + \
+    f"--add-data {joinpath('.', 'client', 'res', 'Gear.jpg')}{delim}res " + \
+    f"--add-data {joinpath('.', 'client', 'res', f'kmit.{iconext}')}{delim}res " \
     f"--upx-exclude kmit.{iconext}) " + extras + " ".join(argv[1:])
 
 print("Building App:\n")
